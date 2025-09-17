a0690487a2be8864474efd8036804a3ee4af02ce
ros260 <ro260@csiro.au>
2025-09-17 14:44:14 +1000

    added data path to error message

--- src/pygeom/process/load.py
+++ src/pygeom/process/load.py
@@ -341,7 +341,7 @@ class ExecuteGeomTasks():
         
         dfilelist = list(Path(datapath).glob(datafile))
         if  len (dfilelist) < 1:
-            cancelHandler.logMessage("Not found data files for  {} ".format(datafile),LoadGeoms_MESSAGE_CATEGORY, LogHandler.Critical)
+            cancelHandler.logMessage(f"Not found data files for  {datafile} in {datapath}",LoadGeoms_MESSAGE_CATEGORY, LogHandler.Critical)
             return
         # grab the first one 
         with dfilelist[0].open() as fp:
